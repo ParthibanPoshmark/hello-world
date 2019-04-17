d85f0c094f248147d7131201301fdf96c1f56a46
ParthibanPoshmark <your_email@example.com>
2019-04-17 15:45:36 +0530

    addedview

--- jobs/eg1jobs.groovy
+++ jobs/eg1jobs.groovy
@@ -7,3 +7,32 @@ job("example1"){
 		scm 'H/5 * * * *'
 	}
 } 
+
+dashboardView('example') {
+    jobs {
+        regex(/example1/)
+    }
+    columns {
+        status()
+        weather()
+        buildButton()
+    }
+    topPortlets {
+        jenkinsJobsList {
+            displayName('acme jobs')
+        }
+    }
+    leftPortlets {
+        testStatisticsChart()
+    }
+    rightPortlets {
+        testTrendChart()
+    }
+    bottomPortlets {
+        iframe {
+            effectiveUrl('http://example.com')
+        }
+        testStatisticsGrid()
+        buildStatistics()
+    }
+}
