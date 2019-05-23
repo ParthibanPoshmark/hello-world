921f27e51480121e4cb734516724c3e347af4f24
ParthibanPoshmark <your_email@example.com>
2019-05-23 18:32:46 +0530

    cool

--- jobs/growth_data/tatari/ad_spend.groovy
+++ jobs/growth_data/tatari/ad_spend.groovy
@@ -2,5 +2,19 @@ String repo="parthibanposhmark/hello-world"
 
 freeStyleJob('gdf-tatari/gd-tatari-ad_spend') {
 	description("<html>"+
-		"<h1>hello world</h1></html>")
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Fetches spend data, campaign and ad metadata from tatari for the last 10 days "+
+      "</td>"+
+   	"</tr>"+
+    
+    
+  "</table>"+
+"</html>")
 }
