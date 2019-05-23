e0fe5ea674b0ca20e203094d3125eea52acf53fd
ParthibanPoshmark <your_email@example.com>
2019-05-23 14:28:21 +0530

    example

--- eg2jobs.groovy
+++ eg2jobs.groovy
@@ -1,10 +1,10 @@
 String repo="parthibanposhmark/hello-world"
 
-job("project-a/example1"){
+job("project-a/example2"){
 	scm{
 		github repo
 	}
 	triggers{
-		scm 'H/10 * * * *'
+		scm 'H/5 * * * *'
 	}
 }
