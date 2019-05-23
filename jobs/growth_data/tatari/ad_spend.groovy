90cbc5c869328fbc620f74ff1b878e2c3b884aa4
ParthibanPoshmark <your_email@example.com>
2019-05-23 17:02:54 +0530

    sample

---
+++ jobs/growth_data/tatari/ad_spend.groovy
@@ -0,0 +1,16 @@
+String repo="parthibanposhmark/hello-world"
+
+freeStyleJob('gdf-tatari/gd-tatari-ad_spend') {
+	description("  <pre>
+
+
+<b>Description	:</b>	Fetches spend data, campaign and ad metadata from tatari for the last 10 days
+<b>Updates Table	:</b> 	analytics.dw_tatari_spend, analytics.dw_growth_campaign, analytics.dw_growth_ad, analytics.dw_acquisition_spend
+<b>Owner		:</b> 	kamal@poshmark.com, parthiban@poshmark.com
+<b>Rake File	:</b>	tatari/ad_spend.rake
+<b>Git		:</b>	<a href='https://github.com/GoshPosh/automator/tree/GROWTH-295_Tags_Metadata_Project'>link</a>
+<b>JIRA		:</b>	<a href='https://poshmark.atlassian.net/browse/GROWTH-295'>link</a>
+  </pre>
+</html>")
+
+}
