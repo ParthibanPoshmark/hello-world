7a8cc9cf69d21c6e1abd8c4e00c327053a006c74
ParthibanPoshmark <your_email@example.com>
2019-05-24 14:40:52 +0530

    egft

--- jobs/growth_data/tatari/ad_spend.groovy
+++ jobs/growth_data/tatari/ad_spend.groovy
@@ -79,6 +79,9 @@ freeStyleJob('gdf-tatari/gd-tatari-ad_spend') {
      git{
       remote{
         url('https://github.com/ParthibanPoshmark/hello-world')
+        name('hello')
+        refspec('defe')
+        credentials('fgrhr')
       }
      }
   }
