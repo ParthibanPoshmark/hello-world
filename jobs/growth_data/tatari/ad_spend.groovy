7d8540f31e5a8f002987f3516a06f9269f8b4e8d
ParthibanPoshmark <your_email@example.com>
2019-05-24 11:10:34 +0530

    seed

--- jobs/growth_data/tatari/ad_spend.groovy
+++ jobs/growth_data/tatari/ad_spend.groovy
@@ -1,10 +1,24 @@
 String repo="parthibanposhmark/hello-world"
 
+folder('gdf-tatari') {
+	displayName('Tatari')
+	views{
+		dashboardView('Dashboard'){
+			topPortlets{
+				buildStatistics{
+					displayName('Build Statistics')
+				}
+			}
+		}
+	}
+}
+
 freeStyleJob('gdf-tatari/gd-tatari-ad_spend') {
 	description("<html>"+
   "<br/>"+
   "<br/>"+
   "<table>"+
+
     "<tr>"+
       "<td style='font-family: Consolas,monospace; '>"+
        "<b>Description	</b>"+	
@@ -14,7 +28,51 @@ freeStyleJob('gdf-tatari/gd-tatari-ad_spend') {
       "</td>"+
    	"</tr>"+
     
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_tatari_spot_booking "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ":	tatari/spot_booking.rake"+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        "<a href='https://github.com/GoshPosh/automator/tree/GROWTH-326-TATARI_NEW_DATA_POINTS'>link</a>"+
+      "</td>"+
+   	"</tr>"+
     
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        "<a href='https://poshmark.atlassian.net/browse/GROWTH-326'>link</a>"+
+      "</td>"+
+   	"</tr>"+
+
   "</table>"+
 "</html>")
 }
