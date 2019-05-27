ea9ad1101ae723281df0de35f467385de5b32d21
ParthibanPoshmark <your_email@example.com>
2019-05-27 14:52:47 +0530

    pinterest

---
+++ jobs/growth_data/pinterest/ad_spend_hourly-sync.groovy
@@ -0,0 +1,94 @@
+freeStyleJob('gdf-pinterest/gd-pinterest-ad_spend_hourly-sync') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls ad spend information from pinterest at hourly level "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_spend_estimates "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": pinterest/ad_spend_hourly.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+  parameters{
+    booleanParam('upload_to_s3', true, null)
+    stringParam('start_date', null , 'YYYY-MM-DD')
+    stringParam('end_date', null, 'YYYY-MM-DD')
+    stringParam('days_back', '0', null)
+    stringParam('last', '0', null)
+  }
+
+  weight(1)
+  
+  label('slave')
+
+  scm{
+     git{
+      branch('*/master')
+      remote{
+        url('https://github.com/ParthibanPoshmark/hello-world')
+      }
+     }
+  }
+
+  wrappers{
+    timeout{
+      elastic(200,3,30)
+    }
+  }
+
+  steps{
+    shell('#!/bin/bash --login -x\n\nbash $WORKSPACE/docker_scripts/spend_estimates/import_spend_estimates_pinterest.sh')
+  }
+
+}
---
+++ jobs/growth_data/pinterest/ad_spend_sync.groovy
@@ -0,0 +1,94 @@
+freeStyleJob('gdf-pinterest/gd-pinterest-ad_spend-sync') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls daily ad spend information from pinterest "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_acquisition_spend "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": pinterest/ad_spend.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+  parameters{
+    booleanParam('upload_to_s3', true, null)
+    stringParam('start_date', null , 'YYYY-MM-DD')
+    stringParam('end_date', null, 'YYYY-MM-DD')
+    stringParam('days_back', '0', null)
+    stringParam('last', '0', null)
+  }
+
+  weight(1)
+  
+  label('slave')
+
+  scm{
+     git{
+      branch('*/master')
+      remote{
+        url('https://github.com/ParthibanPoshmark/hello-world')
+      }
+     }
+  }
+
+  wrappers{
+    timeout{
+      elastic(200,3,30)
+    }
+  }
+
+  steps{
+    shell('#!/bin/bash --login -x\n\nbash $WORKSPACE/docker_scripts/pintrest/foresee_pinterest_api_promoted_pins.sh')
+  }
+
+}
