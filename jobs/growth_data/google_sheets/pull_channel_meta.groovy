0e46a5435eaf482a11f0879c8221b9a5a2b88431
ParthibanPoshmark <your_email@example.com>
2019-05-28 15:21:11 +0530

    chaneel

---
+++ jobs/growth_data/google_sheets/pull_channel_meta.groovy
@@ -0,0 +1,94 @@
+freeStyleJob('gdf-google_sheets/gd-google_sheets-pull_channel_meta') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls meta information about the channel such as growth_unit, acq_channel_group, platform, fixed_cpi, spend_cap etc., from channel_mappings google sheet "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_acq_channel_meta "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ":	google_sheets/pull_channel_meta.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+  logRotator(-1, 30, -1, -1)
+
+  parameters{
+    booleanParam('upload_to_s3', true, null)
+  }
+
+  weight(1)
+  
+  label('slave')
+
+  disabled(true)
+
+  scm{
+     git{
+      branch('*/master')
+      remote{
+        url('https://github.com/ParthibanPoshmark/hello-world')
+      }
+     }
+  }
+
+  authenticationToken('cytokinestorm')
+  
+  triggers{
+    cron('H 0,6,8,12,16,20 * * *')
+  }
+
+  steps{
+    shell('#!/bin/bash --login -x\n\nbash $WORKSPACE/docker_scripts/analytics/push_channel_meta_to_redshift.sh')
+  }
+
+}
