e05e004934302709c912a2202281c2ad331ebd6e
ParthibanPoshmark <your_email@example.com>
2019-05-29 04:33:47 -0700

    google_sync

---
+++ jobs/growth_data/google/ad_spend_sync.groovy
@@ -0,0 +1,97 @@
+freeStyleJob('gdf-google/gd-google-ad_spend-sync') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls campaign level spend data from google "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_acquisition_spend "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ":	google/ad_spend.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+  parameters{
+    stringParam('start_date', null , 'YYYY-MM-DD')
+    stringParam('end_date', null , 'YYYY-MM-DD')
+    stringParam('days_back', '0', null)
+    booleanParam('upload_to_s3', true, null)
+    stringParam('last', '0', null)
+  }
+
+  weight(1)
+  
+  label('slave')
+
+  disabled(true) //Its disabled
+  
+  scm{
+     git{
+      branch('*/master')
+      remote{
+        url('https://github.com/ParthibanPoshmark/hello-world')
+      }
+     }
+  }
+  
+  wrappers {
+    timeout {
+      absolute(10)
+    }
+  }
+
+  steps{
+    shell('#!/bin/bash --login -x\n\nbash $WORKSPACE/docker_scripts/google_adwords/google_adwords_auto_spend_import.sh')
+  }
+
+}
