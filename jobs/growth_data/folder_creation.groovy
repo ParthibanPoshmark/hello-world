e3d8262557f65c36fee6efe0515a8bd112d711f2
ParthibanPoshmark <your_email@example.com>
2019-05-31 21:55:34 -0700

    added_all

--- jobs/growth_data/folder_creation.groovy
+++ jobs/growth_data/folder_creation.groovy
@@ -1,3 +1,36 @@
 folder('gdf-tatari') {
 	displayName('Tatari')
 }
+
+folder('gdf-snapchat'){
+	displayName('Snapchat')
+}
+
+folder('gdf-pinterest'){
+	displayName('Pinterest')
+}
+
+folder('gdf-other_partners'){
+	displayName('Other Partners')
+}
+
+folder('gdf-multi_job'){
+	displayName('MultiJob')
+}
+
+folder('gdf-homeland'){
+	displayName('Homeland')
+}
+
+folder('gdf-google_sheets'){
+	displayName('Google Sheets')
+}
+
+folder('gdf-google'){
+	displayName('Google')
+}
+
+folder('gdf-etl'){
+	displayName('ETL')
+}
+
