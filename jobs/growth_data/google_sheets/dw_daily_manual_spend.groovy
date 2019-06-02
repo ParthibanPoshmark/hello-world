178df9a6948e875ae723806a1bbbf716a776bf30
ParthibanPoshmark <your_email@example.com>
2019-06-02 04:03:26 -0700

    added

---
+++ jobs/growth_data/google_sheets/dw_daily_manual_spend.groovy
@@ -0,0 +1,97 @@
+freeStyleJob('gdf-google_sheets/gd-google_sheets-dw_daily_manual_spend') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls spend data for <b>date+channel+platform+channel_group</b> combination from dw_acquisition_spend then overrides the same if manual spend data is present in spend overrides google sheet and finally updates it in dw_daily_manual_spend "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_daily_manual_spend "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ":	google_sheets/dw_daily_manual_spend.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+  logRotator(-1, 30, -1, -1)
+
+  parameters{
+    booleanParam('upload_to_s3', true, 'Default is true')
+    stringParam('doc_key', '19rtsxOXKgJ48x6bydeFoEnk75Jifbs3H7qcd1rgswn4', null)
+  }
+
+  weight(1)
+  
+  label('slave')
+
+  disabled(true)
+
+  scm{
+     git{
+      branch('*/master')
+      remote{
+        url('https://github.com/ParthibanPoshmark/hello-world')
+      }
+     }
+  }
+
+  triggers{
+    cron('H H/4 * * *')
+  }
+
+  wrappers{
+  	 buildUserVars()
+  }
+
+  steps{
+    shell('#!/bin/bash --login -x\n\nbash $WORKSPACE/docker_scripts/spend/auto_import_manual_spend.sh')
+  }
+
+}
