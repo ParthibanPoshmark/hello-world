0d716d3b5e4fb5ce393bcc43338e72e1443e13c9
ParthibanPoshmark <your_email@example.com>
2019-06-03 00:14:20 -0700

    added again

---
+++ jobs/growth_data/appsflyer/install_data_3_days.groovy
@@ -0,0 +1,65 @@
+freeStyleJob('gdf-appsflyer/gd-appsflyer-install_data-3_days') {
+	description("<html>"+
+  "<br/>"+
+  "<br/>"+
+  "<table>"+
+
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Description	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": Pulls app installation data such as installation time, id, channel, attribution_service etc., from Appsflyer for the last 3 days "+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Updates Table	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	analytics.dw_install_attribution "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Owner	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": 	kamal@poshmark.com, parthiban@poshmark.com "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Rake File	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ":	appsflyer/install_data.rake "+
+      "</td>"+
+   	"</tr>"+
+
+   	"<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>Git	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+    
+    "<tr>"+
+      "<td style='font-family: Consolas,monospace; '>"+
+       "<b>JIRA	</b>"+	
+      "</td>"+
+      "<td style='font-family: Consolas,monospace;'>"+
+        ": NA"+
+      "</td>"+
+   	"</tr>"+
+
+  "</table>"+
+"</html>")
+
+
+}
